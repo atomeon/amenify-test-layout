34d4ba708ef43dd7e0b40d2b7c456b39a7e633c6
atomeon <atomeon@gmail.com>
2022-05-21 21:15:13 +0600

    feat: add test component

---
+++ src/components/testLayout/testLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './testLayout.module.css';
+
+export const TestLayout = () => {
+	return (
+		<div className={styles.testLayout}>
+			Hello world!
+		</div>
+	);
+}
+
