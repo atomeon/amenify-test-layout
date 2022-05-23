df436329c44fa8e46e4b20cfb8bb1b859e9f8ada
atomeon <atomeon@gmail.com>
2022-05-23 14:52:57 +0600

    feat: info panel, rework main laouyt

---
+++ src/components/infoPanel/infoPanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './infoPanel.module.css';
+
+export const InfoPanel = () => {
+	const name = 'Kaylie';
+	return (
+		<section className={styles.infoPanel}>
+			<div>Hello, {name}</div>
+			<div>Wallet credits: <span>$15</span></div>
+		</section>
+	);
+}
+
